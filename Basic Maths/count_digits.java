5b72a4baebd7c7892178d92bf8312dd26bca3e29
Basu <basavarajumachagi99@gmail.com>
2026-03-09 21:29:47 +0530

    Problem1

---
+++ Basic Maths/count_digits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class count_digits{
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int n=sc.nextInt();
+    sc.close();
+    int count=0;
+    while(n>0){
+        n=n/10;
+        count++;
+
+    }
+    System.out.println("The number of digits are : "+count);
+    
+}
+
+
+}
